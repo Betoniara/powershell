2bb50c096b576aed0d79a096dd6ab4a8a5910961
Damian <damian.biedziak@enea.pl>
2025-01-02 14:25:51 +0100

    zmiana

--- OneLiner_RestartComputersbyOU.ps1
+++
@@ -1,3 +0,0 @@
-Get-ADComputer -SearchBase 'CN=Computers,DC=domain,DC=local' -Filter '*' | Select -Exp Name | %{Restart-Computer -ComputerName $_.Name -Force -Confirm}
-
-restart-computer (get-content c:\TEST\komp.txt) -Force
